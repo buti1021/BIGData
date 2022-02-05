7b0ea3deb3bf5d60ac3e0ef1829638bb4ef04d28
tbudras <tbudras1704@aol.com>
2022-02-05 15:10:37 +0100

    small spark job example

---
+++ spark_job.py
@@ -0,0 +1,13 @@
+from pyspark.sql.session import SparkSession
+import pandas as pd
+from pyspark.sql.types import StringType
+
+path = "/opt/conda/miniconda3/lib/python3.8/site-packages/irsx/CSV/index_2021.csv"
+
+df21 = pd.read_csv(path, index_col=False, dtype=str, nrows= 50) # read all as string, not beautiful but we only need object id anyways
+df21.head()
+spark = SparkSession.builder.getOrCreate()
+sdf = spark.createDataFrame(df21["OBJECT_ID"], StringType())
+sdf.explain()
+anz = sdf.count()
+sdf.toPandas().to_csv(f"BIGData/{anz}.csv", index=None)
